dff53f727f5f62045adf216e613671588d26c746
Nam Nguyen Anh <anhnamnguyen99@gmail.com>
2022-01-09 16:02:32 +0700

    [ADD] graphql

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,14 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import path from "path";
+import { Post, User } from "./entities";
 
 export default {
   migrations: {
     path: path.join(__dirname, "./migration"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[jt]s$/, // regex pattern for the migration files
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lireddit",
   type: "postgresql",
   user: "postgres",
