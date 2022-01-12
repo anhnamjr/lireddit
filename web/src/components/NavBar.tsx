a4c68ef98fb01e49b3f4a5d8f10d73e8fccc42fd
Nam Nguyen Anh <anhnamnguyen99@gmail.com>
2022-01-12 22:42:02 +0700

    [UPDATE] logout feature

---
+++ web/src/components/NavBar.tsx
@@ -0,0 +1,54 @@
+import { Box, Flex, Link, Button } from "@chakra-ui/react";
+import NextLink from "next/link";
+import React from "react";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  const [{ fetching: fetchingLogout }, logout] = useLogoutMutation();
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    //user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+    //user is logged in
+  } else {
+    body = (
+      <Flex>
+        <Box>{data?.me?.username}</Box>
+        <Button
+          isLoading={fetchingLogout}
+          ml={2}
+          variant="link"
+          onClick={handleLogout}
+        >
+          Logout
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tan" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
