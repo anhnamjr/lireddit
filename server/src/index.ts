119e81d48d5e9346b53a4def2587979652fa538b
Nam Nguyen Anh <anhnamnguyen99@gmail.com>
2022-01-12 22:46:33 +0700

    [REPLACE] name of cookie

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { PostResolver } from "./resolvers/post";
 import { HelloResolver } from "./resolvers/hello";
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -38,7 +38,7 @@ const main = async () => {
 
   app.use(
     session({
-      name: "qid",
+      name: COOKIE_NAME,
       store: new RedisStore({ client: redisClient, disableTouch: true }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
